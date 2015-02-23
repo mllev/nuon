df5e8dd7399e455a9d0165425cd76b46b0831edc
matthew levenstein <matthewlevenstein@gmail.com>
2015-02-23 18:27:18 -0500

    better strlen

--- src/nuon.c
+++ src/nuon.c
@@ -42,23 +42,29 @@ int height ();
 map_node_t* map_node_init (int, const char*, void*);
 void map_node_destroy (map_node_t *);
 
-static inline int strncmpsafe (unsigned char* k0, unsigned char* k1)
+static inline int nuonStrlen (unsigned char * str)
+{
+  size_t len, nlen;
+
+  len = strlen((const char *)str);
+  nlen = len > 256 ? 256 : len;
+  str[nlen] = 0;
+  return nlen;
+}
+
+static inline int nuonStrncmp (unsigned char* k0, unsigned char* k1)
 {
   int kl0, kl1, kl, res;
 
-  kl0 = strlen((const char *)k0);
-  kl1 = strlen((const char *)k1);
+  kl0 = nuonStrlen(k0);
+  kl1 = nuonStrlen(k1);
   kl = (kl0 < kl1 ? kl0 : kl1);
   res = strncmp((const char*)k0, (const char*)k1, kl);
 
   if ( !res ) {
-    if ( kl0 == kl1 ) {
-      return res;
-    } else if ( kl0 < kl1 ) {
-      return -1;
-    } else {
-      return 1;
-    }
+    if ( kl0 == kl1 ) { return 0; } 
+    else if ( kl0 < kl1 ) { return -1; }
+    else { return 1; }
   } else {
     return res;
   }
@@ -122,7 +128,7 @@ int map_remove (map_t* m, const char* k)
   map_node_t* del;
 
   while ( --h >= 0 ) {
-    while ( iter->next[h] && strncmpsafe((unsigned char *)k, iter->next[h]->key) < 0 ) {
+    while ( iter->next[h] && nuonStrncmp((unsigned char *)k, iter->next[h]->key) < 0 ) {
       iter = iter->next[h];
     }
 
@@ -131,11 +137,11 @@ int map_remove (map_t* m, const char* k)
 
   del = update[0]->next[0];
 
-  if ( iter->next[0] && !strncmpsafe((unsigned char *)k, iter->next[0]->key) ) {
+  if ( iter->next[0] && !nuonStrncmp((unsigned char *)k, iter->next[0]->key) ) {
     h = m->height;
 
     while ( i < h ) {
-      if ( strncmpsafe((unsigned char *)k, update[i]->next[i]->key) ) {
+      if ( nuonStrncmp((unsigned char *)k, update[i]->next[i]->key) ) {
         break;
       }
 
@@ -162,14 +168,14 @@ int map_set (map_t* m, const char* k, void* v)
   map_node_t* n;
 
   while ( --h >= 0 ) {
-    while ( iter->next[h] && strncmpsafe((unsigned char *)k, iter->next[h]->key) < 0 ) {
+    while ( iter->next[h] && nuonStrncmp((unsigned char *)k, iter->next[h]->key) < 0 ) {
       iter = iter->next[h];
     }
 
     update[h] = iter;
   }
 
-  if ( iter->next[0] && !strncmpsafe((unsigned char *)k, iter->next[0]->key) ) {
+  if ( iter->next[0] && !nuonStrncmp((unsigned char *)k, iter->next[0]->key) ) {
     free(iter->next[0]->data);
     iter->next[0]->data = v;
     return 1;
@@ -203,12 +209,12 @@ void* map_get (map_t* m, const char* k)
   int klen = strlen(k);
 
   while (--h >= 0) {
-    while ( iter->next[h] && strncmpsafe((unsigned char *)k, iter->next[h]->key) < 0 ) {
+    while ( iter->next[h] && nuonStrncmp((unsigned char *)k, iter->next[h]->key) < 0 ) {
       iter = iter->next[h];
     }
   }
 
-  if ( !iter->next[0] || strncmpsafe((unsigned char *)k, iter->next[0]->key) != 0 ) {
+  if ( !iter->next[0] || nuonStrncmp((unsigned char *)k, iter->next[0]->key) != 0 ) {
     return NULL;
   }
 
@@ -486,7 +492,7 @@ void graph_vertexSetProperty (Vertex* vertex, unsigned char* key, unsigned char*
   }
 
   while (iter->next) {
-    if (!strncmpsafe(iter->next->key, key)) {
+    if (!nuonStrncmp(iter->next->key, key)) {
       exists = 1;
       break;
     }
@@ -498,7 +504,7 @@ void graph_vertexSetProperty (Vertex* vertex, unsigned char* key, unsigned char*
     iter->next = del->next;
     property_destroy(del);
   } else {
-    if (!strncmpsafe(iter->key, key)) {
+    if (!nuonStrncmp(iter->key, key)) {
       vertex->properties = property_init(key, val);
       vertex->properties->next = iter->next;
       property_destroy(iter);
@@ -517,7 +523,7 @@ unsigned char* graph_vertexGetProperty (Vertex* vertex, unsigned char* key)
   int kl = strlen((const char *)key);
 
   while ( iter ) {
-    if (!strncmpsafe(iter->key, key)) {
+    if (!nuonStrncmp(iter->key, key)) {
       break;
     }
     iter = iter->next;
@@ -542,7 +548,7 @@ void graph_vertexRemoveProperty (Vertex* vertex, unsigned char* key)
   iter = vertex->properties;
 
   /* if it's the first one to be deleted */
-  if ( !strncmpsafe(iter->key, key)) {
+  if ( !nuonStrncmp(iter->key, key)) {
     del = iter;
     vertex->properties = iter->next;
     property_destroy(del);
@@ -550,7 +556,7 @@ void graph_vertexRemoveProperty (Vertex* vertex, unsigned char* key)
   }
 
   while ( iter->next ) {
-    if ( !strncmpsafe(iter->next->key, key) ) {
+    if ( !nuonStrncmp(iter->next->key, key) ) {
       break;
     }
     iter = iter->next;
@@ -598,7 +604,7 @@ VertexContainer* graph_getVertices (Graph* g, unsigned char* label, unsigned cha
       if ( key ) {
         prop = graph_vertexGetProperty(edge_iter->to, key);
       }
-      if ( !key || (prop && !strncmpsafe(prop, val)) ) {
+      if ( !key || (prop && !nuonStrncmp(prop, val)) ) {
         if ( !head ) {
           head = graph_vertexContainerInit(edge_iter->to);
           tail = head;
@@ -618,7 +624,7 @@ VertexContainer* graph_getVertices (Graph* g, unsigned char* label, unsigned cha
         if ( key ) {
           prop = graph_vertexGetProperty(ptr->data, key);
         }
-        if ( !key || (prop && !strncmpsafe(prop, val)) ) {
+        if ( !key || (prop && !nuonStrncmp(prop, val)) ) {
           if ( !head ) {
             head = graph_vertexContainerInit(ptr->data);
             tail = head;
@@ -1159,12 +1165,6 @@ void parse (Graph* g, unsigned char* p)
   _expr(g, &data);
 }
 
-static inline int strlensafe (unsigned char *str)
-{
-  int l = strlen((const char *)str);
-  return (l > 512 ? 512 : l);
-}
-
 node_data_t* exec_addNode(node_data_t* root, unsigned char* ident) 
 {
   int len;
@@ -1173,7 +1173,7 @@ node_data_t* exec_addNode(node_data_t* root, unsigned char* ident)
   node = malloc(sizeof(node_data_t));
 
   if ( ident ) { 
-    len = strlensafe(ident);
+    len = nuonStrlen(ident);
     strncpy((char *)node->ident, (char *)ident, len);
     node->ident[len] = 0;
   } else {
@@ -1201,9 +1201,9 @@ node_set_data_t* exec_addNodeUpdate(node_set_data_t* root, unsigned char* ident,
   int ilen, klen, vlen;
   node_set_data_t* node;
 
-  ilen = strlensafe(ident);
-  klen = strlensafe(key);
-  vlen = strlensafe(value);
+  ilen = nuonStrlen(ident);
+  klen = nuonStrlen(key);
+  vlen = nuonStrlen(value);
 
   node = malloc(sizeof(node_set_data_t));
 
@@ -1234,9 +1234,9 @@ edge_set_data_t* exec_addEdgeUpdate(
   int len, llen, rlen;
   edge_set_data_t* edge;
 
-  llen = strlensafe(left);
-  rlen = strlensafe(right);
-  len = strlensafe(label);
+  llen = nuonStrlen(left);
+  rlen = nuonStrlen(right);
+  len = nuonStrlen(label);
 
   edge = malloc(sizeof(edge_set_data_t));
 
@@ -1262,7 +1262,7 @@ void exec_addLabelToNode(node_data_t* node, unsigned char* label)
 {
   int len;
 
-  len = strlensafe(label);
+  len = nuonStrlen(label);
   strncpy((char *)node->label, (char *)label, len);
   node->label[len] = 0;
 }
@@ -1272,7 +1272,7 @@ node_data_t* exec_findNode(node_data_t* root, unsigned char* ident)
   int len;
   node_data_t* iter;
 
-  len = strlensafe(ident);
+  len = nuonStrlen(ident);
   iter = root;
 
   while ( iter ) {
@@ -1288,8 +1288,8 @@ void exec_addProperty(node_data_t* node, unsigned char* key, unsigned char* val)
 {
   int klen, vlen, index;
 
-  klen = strlensafe(key);
-  vlen = strlensafe(val);
+  klen = nuonStrlen(key);
+  vlen = nuonStrlen(val);
   index = node->propcount;
 
   strncpy((char *)node->keys[index], (char *)key, klen);
@@ -1309,7 +1309,7 @@ edge_data_t* exec_addEdge(edge_data_t* root, unsigned char* ident)
   edge = malloc(sizeof(edge_data_t));
 
   if ( ident ) {
-    len = strlensafe(ident);
+    len = nuonStrlen(ident);
     strncpy((char *)edge->ident, (char *)ident, len);
     edge->ident[len] = 0;
   }
@@ -1340,7 +1340,7 @@ void exec_addLabelToEdge(edge_data_t* edge, unsigned char* label)
 {
   int len;
 
-  len = strlensafe(label);
+  len = nuonStrlen(label);
   strncpy((char *)edge->label, (char *)label, len);
   edge->label[len] = 0;
 }
