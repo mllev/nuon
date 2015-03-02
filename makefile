19e675f2872359147eab45eebd976421d6a645be
matthew levenstein <matthewlevenstein@gmail.com>
2015-03-02 17:35:15 -0500

    added event lib

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ CC=gcc
 all: build
 
 build:
-	$(CC) src/main.c src/nuon.c src/linenoise.c -o bin/nuon
+	$(CC) src/main.c src/nuon.c src/picoev_kqueue.c -o bin/nuon
 
 clean:
 	rm -rf bin/nuon
--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
-/* nuon
+/* NUON - Graph Database
+ * This file is basically a modified version of the main picoev example file
+ *
+ *        / /|\ \ 
+ *     / / /|||\ \ \
+ *    | | |     | | |
+ *    | | |     | | |
+ *    \ \ \     / / /
+ *     \ \ \   / / /
+ *      \ \ \ / / /
+ *       \ \ \ / /
+ *        \ \ \ /  For my beautiful sister Holly and her triumphant battle against cancer. 
+ *        /\ \ \   Your courage is truly humbling.
+ *       / /\ \ \
+ *      / / /\ \ \
+ *     / / /  \ \ \
+ *    / / /    \ \ \
+ *     / /      \ \ \
+ *               \ \ 
  *
  * Copyright (c) 2015, Matthew Levenstein
  * All rights reserved.
@@ -28,68 +46,122 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include "picoev.h"
 
-#include "linenoise.h"
-#include "nuon.h"
+//#include "nuon.h"
 
-void teststr(char * str);
-void parserTest (void);
-char* readline (FILE*);
+#define HOST 0 /* 0x7f000001 for localhost */
+#define PORT 23456
+#define MAX_FDS 1024
+#define TIMEOUT_SECS 10
 
-char* readline (FILE* f)
+// Graph nuon;
+
+static void setup_sock(int fd)
 {
-  unsigned char* buf = NULL;
-  int c = 0, i = 0, bufsize = 10;
+  int on = 1, r;
 
-  buf = malloc(bufsize + 1);
-  memset(buf, 0, bufsize + 1);
+  r = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
+  assert(r == 0);
+  r = fcntl(fd, F_SETFL, O_NONBLOCK);
+  assert(r == 0);
+}
 
-  while ( (c = fgetc(f)) != EOF ) {
-    if ( i == bufsize ) {
-      buf = realloc(buf, (bufsize += 10) + 1);
-    }
-    buf[i++] = (unsigned char)c;
-    if ( buf[i - 1] == '\n' ) {
+static void close_conn(picoev_loop* loop, int fd)
+{
+  picoev_del(loop, fd);
+  close(fd);
+  printf("closed: %d\n", fd);
+}
+
+static void rw_callback(picoev_loop* loop, int fd, int events, void* cb_arg)
+{
+  if ((events & PICOEV_TIMEOUT) != 0) {
+    
+    /* timeout */
+    close_conn(loop, fd);
+    
+  } else if ((events & PICOEV_READ) != 0) {
+    
+    /* update timeout, and read */
+    char buf[1024];
+    ssize_t r;
+    picoev_set_timeout(loop, fd, TIMEOUT_SECS);
+    r = read(fd, buf, sizeof(buf));
+    switch (r) {
+    case 0: /* connection closed by peer */
+      close_conn(loop, fd);
+      break;
+    case -1: /* error */
+      if (errno == EAGAIN || errno == EWOULDBLOCK) { /* try again later */
+  break;
+      } else { /* fatal error */
+  close_conn(loop, fd);
+      }
+      break;
+    default: /* got some data, send back */
+      // parse(&nuon, buf);
+      if (write(fd, buf, r) != r) {
+        close_conn(loop, fd);
+      }
       break;
     }
   }
-
-  buf[i] = 0;
-
-  return (char *)buf;
 }
 
-
-void parserTest (void)
+static void accept_callback(picoev_loop* loop, int fd, int events, void* cb_arg)
 {
-  Graph* g = graph_init(100000);
-  char* line;
-  while((line = linenoise("nuon> ")) != NULL) {
-    parse(g, (unsigned char *)line);
-    linenoiseHistoryAdd(line);
-    free(line);
+  int newfd = accept(fd, NULL, NULL);
+  if (newfd != -1) {
+    printf("connected: %d\n", newfd);
+    setup_sock(newfd);
+    picoev_add(loop, newfd, PICOEV_READ, TIMEOUT_SECS, rw_callback, NULL);
   }
-  // while ( 1 ) {
-  //   printf("nuon> ");
-  //   line = readline(stdin);
-  //   parse(g, (unsigned char *)line);
-  //   free(line);
-  //   line = NULL;
-  // }
 }
 
-void teststr(char * str)
+int main(void)
 {
-  static int i = 0;
-  sprintf(str, "%d", ++i);
-}
-
-int main (void)
-{
-  parserTest();
+  picoev_loop* loop;
+  int listen_sock, flag;
+  
+  /* listen to port */
+  assert((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) != -1);
+  flag = 1;
+  assert(setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag))
+   == 0);
+  struct sockaddr_in listen_addr;
+  listen_addr.sin_family = AF_INET;
+  listen_addr.sin_port = htons(PORT);
+  listen_addr.sin_addr.s_addr = htonl(HOST);
+  assert(bind(listen_sock, (struct sockaddr*)&listen_addr, sizeof(listen_addr))
+   == 0);
+  assert(listen(listen_sock, 5) == 0);
+  setup_sock(listen_sock);
+  
+  /* init picoev */
+  picoev_init(MAX_FDS);
+  /* create loop */
+  loop = picoev_create_loop(60);
+  /* add listen socket */
+  picoev_add(loop, listen_sock, PICOEV_READ, 0, accept_callback, NULL);
+  /* loop */
+  printf("Welcome to NUON.\nListening for TCP connections on port %d\n...", PORT);
+  while (1) {
+    picoev_loop_once(loop, 10);
+  }
+  /* cleanup */
+  picoev_destroy_loop(loop);
+  picoev_deinit();
+  
   return 0;
 }
